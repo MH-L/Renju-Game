cd286ba65ca580357e119f63b737d3a9d62ede6c
Kelvin Yip <autilite@gmail.com>
2015-04-10 18:22:35 -0700

    Format main class

--- src/Application/Application/Main.java
+++ src/Application/Application/Main.java
@@ -12,81 +12,65 @@ public class Main {
 
 	public static void main(String[] args) {
 		Scanner reader = new Scanner(System.in);
-		System.out.println(" Welcome to the Renju Game!\n Select a game mode:\n " +
-				" (" + Game.MULTIPLAYER_GAME_MODE + ") multiplayer\n " +
-				" (" + Game.SINGLEPLAYER_GAME_MODE + ") singleplayer");
-		String inputStream = reader.next();
-		while (!inputStream.equals(String.valueOf(Game.MULTIPLAYER_GAME_MODE)) &&
-				!inputStream.equals(String.valueOf(Game.SINGLEPLAYER_GAME_MODE))) {
-			System.out.println("Invalid input. Please re-enter your choice.");
-			inputStream = reader.next();
-		}
+		String inputStream = getGameMode(reader);
 		// TODO get difficulty from the user
 		game = Game.getInstance(Integer.parseInt(inputStream), 4);
+		printInstruction();
 		game.getBoard().renderBoard();
-		boolean counter = true;
+		boolean isPlayer1 = true;
 		String dispStr;
 		// note that the y-coord is the first, and the x-coord is the second;
 		// to comply with the indices of the grid.
 		// also notice that the location is always 1-based (user-friendly).
 		while (!boardFull(game.getBoard()) && !isWinning(game.getBoard())) {
-			if (counter)
-				dispStr = "First player, ";
+			if (isPlayer1)
+				dispStr = "one";
 			else
-				dispStr = "Second player, ";
+				dispStr = "two";
 
-			System.out.println(dispStr + "now it is your turn to enter the place you want to\n insert the"
-					+ " peg, use two numbers ranging from 1 to 16,\n and separate them using"
-					+ " a comma.\n Note that the first one is the x-coordinate,\n and the"
-					+ " second one is the y-coordinate. If you want to withdraw\n"
-					+ " your last choice, enter w. If you want to exit the game,"
-					+ " enter x.");
+			System.out.println("Player " + dispStr + ", it is your turn.");
 			inputStream = reader.next();
-			if (inputStream.equals("x")) {
-				System.out.println("Game Over!");
-				reader.close();
-				return;
-			} else if (inputStream.equals("w")) {
-				try {
-					game.getPlayer1().withdraw();
-				} catch (WithdrawException e) {
-					// TODO Auto-generated catch block
-					System.out.println(e.getMessage());
-				}
+			switch (inputStream){
+				case "x":
+					actionGameOver(reader);
+					return;
+				case "w":
+					try {
+						game.getPlayer1().withdraw();
+					} catch (WithdrawException e) {
+						System.out.println(e.getMessage());
+						continue;
+					}
+					break;
+				case "i":
+					printInstruction();
+					break;
 			}
 			if (inputStream.contains(",")) {
 				inputStream = inputStream.trim();
 				String[] inputs = inputStream.split(",");
-				if (inputs.length == 2) {
-					int x_coord = translate(inputs[0]);
-					int y_coord = Integer.parseInt(inputs[1]);
-					BoardLocation toPlace = new BoardLocation(y_coord - 1, x_coord - 1);
-					// TODO handle isReachable by exception
-					if (!toPlace.isReachable()) {
-						System.out.println("The input you entered is not valid.\n All coordinates"
-								+ " must be between 1 and 16.");
-						continue;
-					} else {
-						boolean success;
-						if (counter)
-							success = game.getBoard().updateBoard(toPlace, true);
-						else
-							success = game.getBoard().updateBoard(toPlace, false);
-						if (!success) {
-							System.out.println("The move is invalid. Please try another.");
-							continue;
-						} else {
-							// Succeeded.
-							game.getBoard().renderBoard();
-						}
-
-					}
-				} else {
+				if (inputs.length != 2) {
 					System.out.println("The input you entered is invalid!");
 					continue;
 				}
+				int x_coord = translate(inputs[0]);
+				int y_coord = Integer.parseInt(inputs[1]);
+				BoardLocation toPlace = new BoardLocation(y_coord - 1, x_coord - 1);
+				// TODO handle isReachable by exception
+				if (!toPlace.isReachable()) {
+					System.out.println("The input you entered is not valid.\n All coordinates"
+							+ " must be between 1 and 16.");
+					continue;
+				};
+				boolean success = game.getBoard().updateBoard(toPlace, isPlayer1);
+				if (!success) {
+					System.out.println("The move is invalid. Please try another.");
+					continue;
+				} else {
+					game.getBoard().renderBoard();
+				}
 			}
-			counter = !counter;
+			isPlayer1 = !isPlayer1;
 		}
 		if (isWinning(game.getBoard())){
 			System.out.println("You won!");
@@ -94,16 +78,22 @@ public class Main {
 		reader.close();
 	}
 
-	public static void showboard() {
-		System.out.println("Here is the initial board.");
-		// Prints out the initial board.
-		// Note that player's pegs are denoted by an blank circle;
-		// while the AI's pegs are denoted by a solid circle.
-		// Board locations which are unoccupied are denoted by
-		// a empty square.
-		for (int i = 0; i < 16; i++)
-			System.out.println("\u25A1\u25A1\u25A1\u25A1\u25A1\u25A1\u25A1\u25A1"
-					+ "\u25A1\u25A1\u25A1\u25A1\u25A1\u25A1\u25A1\u25A1");
+	private static void actionGameOver(Scanner reader) {
+		System.out.println("Game Over!");
+		reader.close();
+	}
+
+	private static void printInstruction() {
+		// TODO fix the magic number
+		System.out.println("Game instruction:\nEach player takes turn to place a stone on the board." +
+				"\nThe first one to place 5 consecutive stones in a row wins the game." +
+				"\nTo place a stone, select the x-position then y-position delimited by a comma." +
+				"\n  For example: A,1 or B,3." +
+				"\n\nYou are allowed to undo your last move up to 3 times." +
+				"Enter \"w\" to withdraw when it is your turn." +
+				"\nTo exit the game, enter \"x\". " +
+				"\nTo reshow the instruction, enter \"i\"\n");
+
 	}
 
 	public static boolean isWinning(Board board) {
@@ -116,5 +106,18 @@ public class Main {
 
 	public static int translate(String letter) {
 		return letter.toLowerCase().toCharArray()[0] - 96;
+	}
+
+	private static String getGameMode(Scanner reader) {
+		System.out.println(" Welcome to the Renju Game!\n Select a game mode:\n " +
+				" (" + Game.MULTIPLAYER_GAME_MODE + ") multiplayer\n " +
+				" (" + Game.SINGLEPLAYER_GAME_MODE + ") singleplayer");
+		String gameMode = reader.next();
+		while (!gameMode.equals(String.valueOf(Game.MULTIPLAYER_GAME_MODE)) &&
+				!gameMode.equals(String.valueOf(Game.SINGLEPLAYER_GAME_MODE))) {
+			System.out.println("Invalid input. Please re-enter your choice.");
+			gameMode = reader.next();
+		}
+		return gameMode;
 	}
 }
